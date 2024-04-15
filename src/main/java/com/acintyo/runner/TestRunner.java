7a2b849c85ac8e88dab60665935df792210112d5
charan reddy <gundacharan1054@gmail.com>
2024-04-15 14:17:17 +0530

    Added Ruuner class for Testing

---
+++ src/main/java/com/acintyo/runner/TestRunner.java
@@ -0,0 +1,11 @@
+package com.acintyo.runner;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class TestRunner implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Test Runner class");
+	}
+}
